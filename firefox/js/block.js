58f3e0d634b48cba298c8672a418a77352d956e4
Shin Jinhyeok <hakujitsu@protonmail.ch>
2022-02-27 21:15:17 +0900

    변수명 변경

--- firefox/js/block.js
+++ firefox/js/block.js
@@ -4,32 +4,32 @@ export async function blockArticles(boardType) {
     const blockMemberList = await getBlockMemberList();
 
     for (const blockMember of blockMemberList) {
-        const targetArticleList = [...document.querySelectorAll(`a[onclick^="ui(event, '${blockMember}'"]`)];
-        targetArticleList.forEach((targetArticle, index) => {
-            targetArticleList[index] = targetArticle.closest('li,tr:not(:is(li,td) tr)');
+        const articleElementList = [...document.querySelectorAll(`a[onclick^="ui(event, '${blockMember}'"]`)];
+        articleElementList.forEach((targetArticle, index) => {
+            articleElementList[index] = targetArticle.closest('li,tr:not(:is(li,td) tr)');
         });
 
         if (boardType === 'L') {
-            for (const targetArticle of targetArticleList) {
-                const href = targetArticle.querySelector('a.article').href;
+            for (const articleElement of articleElementList) {
+                const href = articleElement.querySelector('a.article').href;
 
-                targetArticle.innerHTML =
+                articleElement.innerHTML =
                     `<td colspan="5" style="color: #676767;">
                         <a href="${href}">차단한 멤버의 게시글입니다.</a>
                     </td>`;
             }
         }
         else if (boardType === 'C') {
-            for (const targetArticle of targetArticleList) {
-                const href = targetArticle.querySelector('a.tit').href;
+            for (const articleElement of articleElementList) {
+                const href = articleElement.querySelector('a.tit').href;
 
-                targetArticle.innerHTML =
+                articleElement.innerHTML =
                     `<a href="${href}" style="color: #676767;">차단한 멤버의 게시글입니다.</a>`;
             }
         }
         else {
-            for (const targetArticle of targetArticleList) {
-                targetArticle.style.display = 'none';
+            for (const articleElement of articleElementList) {
+                articleElement.style.display = 'none';
             }
         }
     }
