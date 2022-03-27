5f4fa0577b717897c67248bae99d9482c6f77d15
Shin Jinhyeok <hakujitsu@protonmail.ch>
2022-03-28 02:12:17 +0900

    필요 없는 API 삭제, 주석 추가

--- chrome/js/misc/cafe-apis.js
+++ chrome/js/misc/cafe-apis.js
@@ -1,7 +1,17 @@
-async function fetchWithCredentials(url) {
+/**
+ * 쿠키와 함께 요청을 보냅니다.
+ * @param {string} url 요청을 보낼 URL
+ * @return {Promise<any>} 응답
+ */
+ async function fetchWithCredentials(url) {
     return await fetch(url, { credentials: 'include' });
 }
 
+/**
+ * 게시글의 내용을 가져옵니다.
+ * @param {string|number} articleId 게시글 아이디
+ * @returns {Promise<object>} 게시글의 내용
+ */
 export async function getArticle(articleId) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-articleapi/v2/cafes/27842958/articles/${articleId}`);
     const json = await response.json();
@@ -9,6 +19,13 @@ export async function getArticle(articleId) {
     return json.result.article;
 }
 
+/**
+ * 게시글 목록을 가져옵니다.
+ * @param {string|number} menuId 게시판 아이디 (비우면 전체 게시판)
+ * @param {string|number} page 페이지
+ * @param {string|number} perPage 한 페이지당 게시글 개수
+ * @returns {Promise<object>} 게시글 목록
+ */
 export async function getArticleList(menuId, page, perPage) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe2/ArticleList.json?search.clubid=27842958&search.menuid=${menuId}&search.page=${page}&search.perPage=${perPage}`);
     const json = await response.json();
@@ -16,16 +33,16 @@ export async function getArticleList(menuId, page, perPage) {
     return json.message.result.articleList;
 }
 
-export async function getArticleListAndBlockMemberList(menuId, page, perPage) {
-    const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe2/ArticleList.json?search.clubid=27842958&search.menuid=${menuId}&search.page=${page}&search.perPage=${perPage}`);
-    const json = await response.json();
-
-    return {
-        articleList: json.message.result.articleList,
-        blockMemberList: json.message.result.blockMemberList,
-    };
-}
-
+/**
+ * 게시글 검색 목록을 가져옵니다.
+ * @param {string|number} menuId 게시판 아이디 (비우면 전체 게시판)
+ * @param {string|number} page 페이지
+ * @param {string|number} perPage 한 페이지당 게시글 개수
+ * @param {string} query 검색어
+ * @param {string|number} searchBy 검색 기준 (0: 게시글, 1: 제목, 3: 글 작성자, 4: 댓글 내용, 5: 댓글 작성자)
+ * @param {string} sortBy 정렬 기준 (date: 최신순, sim: 정확도순)
+ * @returns {Promise<object>} 게시글 검색 목록
+ */
 export async function getArticleSearchList(menuId, page, perPage, query, searchBy, sortBy) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-mobile/CafeMobileWebArticleSearchListV3?cafeId=27842958&menuId=${menuId}&query=${query}&searchBy=${searchBy}&sortBy=${sortBy}&page=${page}&perPage=${perPage}&adUnit=MW_CAFE_BOARD`);
     const json = await response.json();
@@ -33,6 +50,12 @@ export async function getArticleSearchList(menuId, page, perPage, query, searchB
     return json.message.result.articleList;
 }
 
+/**
+ * 베스트 게시글 목록을 가져옵니다.
+ * @param {string} type 종류 (comment: 댓글, likeIt: 좋아요)
+ * @param {string} period 기간 (week: 일주일, month: 한 달, whole: 전체)
+ * @returns {Promise<object>} 베스트 게시글 목록
+ */
 export async function getBestArticleList(type, period) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe2/BestArticleListV2.json?cafeId=27842958&type=${type}&period=${period}&adUnit=MW_CAFE_BOARD`);
     const json = await response.json();
@@ -40,6 +63,10 @@ export async function getBestArticleList(type, period) {
     return json.message.result.articleList;
 }
 
+/**
+ * 차단 멤버 목록을 가져옵니다.
+ * @returns {Promise<object>} 차단 멤버 목록
+ */
 export async function getBlockMemberList() {
     const response = await fetchWithCredentials('https://apis.naver.com/cafe-web/cafe-cafeinfo-api/v1.0/cafes/27842958/block-members');
     const json = await response.json();
@@ -47,7 +74,14 @@ export async function getBlockMemberList() {
     return json.result;
 }
 
-export async function getCafeMemberArticleList(memberId, perPage, page) {
+/**
+ * 작성 글 목록을 가져옵니다.
+ * @param {string} memberId 멤버 아이디
+ * @param {string|number} page 페이지
+ * @param {string|number} perPage 페이지당 게시글 개수
+ * @returns {Promise<object>} 작성 글 목록
+ */
+export async function getCafeMemberArticleList(memberId, page, perPage) {
     const memberKey = await getMemberKeyByMemberId(memberId);
 
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-mobile/CafeMemberNetworkArticleList?search.cafeId=27842958&search.memberKey=${memberKey}&search.perPage=${perPage}&search.page=${page}`);
@@ -56,7 +90,14 @@ export async function getCafeMemberArticleList(memberId, perPage, page) {
     return json.message.result.articleList;
 }
 
-export async function getCafeMemberCommentList(memberId, perPage, page) {
+/**
+ * 댓글 단 글 목록을 가져옵니다.
+ * @param {string} memberId 멤버 아이디
+ * @param {string|number} page 페이지
+ * @param {string|number} perPage 페이지당 게시글 개수
+ * @returns {Promise<object>} 댓글 단 글 목록
+ */
+export async function getCafeMemberCommentList(memberId, page, perPage) {
     const memberKey = await getMemberKeyByMemberId(memberId);
 
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-mobile/CafeMemberNetworkReplyList?search.clubid=27842958&search.memberKey=${memberKey}&search.perPage=${perPage}&search.page=${page}`);
@@ -65,6 +106,13 @@ export async function getCafeMemberCommentList(memberId, perPage, page) {
     return json.message.result.articleList;
 }
 
+/**
+ * 좋아요한 글 목록을 가져옵니다.
+ * @param {string} memberId 멤버 아이디
+ * @param {string|number} perPage 페이지당 게시글 개수
+ * @param {string|number} likeItTimestamp 검색 시작 타임스탬프
+ * @returns {Promise<object>} 좋아요한 글 목록
+ */
 export async function getCafeMemberLikeItList(memberId, perPage, likeItTimestamp) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-mobile/CafeMemberLikeItListPC?search.cafeId=27842958&search.memberId=${memberId}&search.perPage=${perPage}&search.likeItTimestamp=${likeItTimestamp}`);
     const json = await response.json();
@@ -76,6 +124,10 @@ export async function getCafeMemberLikeItList(memberId, perPage, likeItTimestamp
     };
 }
 
+/**
+ * 자신의 멤버 정보를 가져옵니다. (아이디, 닉네임, 가입 여부 등)
+ * @returns {Promise<object>} 멤버 정보
+ */
 export async function getCafeMemberInfo() {
     const response = await fetchWithCredentials('https://apis.naver.com/cafe-web/cafe2/CafeMemberInfo.json?cafeId=27842958');
     const json = await response.json();
@@ -83,6 +135,11 @@ export async function getCafeMemberInfo() {
     return json.message.result;
 }
 
+/**
+ * 멤버 프로필을 가져옵니다. (방문 수, 작성 글, 작성 댓글 등)
+ * @param {string} memberId 멤버 아이디
+ * @returns {Promise<object>} 멤버 프로필
+ */
 export async function getCafeMemberProfile(memberId) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-mobile/CafeMemberProfile?cafeId=27842958&memberId=${memberId}`);
     const json = await response.json();
@@ -90,6 +147,11 @@ export async function getCafeMemberProfile(memberId) {
     return json.message.result;
 }
 
+/**
+ * 멤버 상태를 가져옵니다. (활동 정지 여부, 강제 탈퇴 여부 등)
+ * @param {string} memberId 멤버 아이디
+ * @returns {Promise<object>} 멤버 상태
+ */
 export async function getCafeMemberStatus(memberId) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-mobile/CafeMemberStatus?cafeId=27842958&memberId=${memberId}`);
     const json = await response.json();
@@ -97,6 +159,12 @@ export async function getCafeMemberStatus(memberId) {
     return json.message.result;
 }
 
+/**
+ * 게시글의 댓글 목록을 가져옵니다.
+ * @param {string|number} articleId 게시글 아이디 
+ * @param {string|number} page 페이지
+ * @returns {Promise<object>} 댓글 목록
+ */
 export async function getCommentList(articleId, page) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-articleapi/cafes/27842958/articles/${articleId}/comments/pages/${page}`);
     const json = await response.json();
@@ -107,6 +175,11 @@ export async function getCommentList(articleId, page) {
     };
 }
 
+/**
+ * 게시글의 모든 댓글 목록을 가져옵니다.
+ * @param {string|number} articleId 게시글 아이디
+ * @returns {Promise<object>} 모든 댓글 목록
+ */
 export async function getCommentListAll(articleId) {
     const commentList = await getCommentList(articleId, 1);
 
@@ -121,6 +194,12 @@ export async function getCommentListAll(articleId) {
     return commentListAll;
 }
 
+/**
+ * 게시글의 최근 댓글 목록을 가져옵니다.
+ * @param {string|number} articleId 게시글 아이디
+ * @param {number} recentPageCount 가져올 최대 페이지 수
+ * @returns {Promise<object>} 최근 댓글 목록
+ */
 export async function getCommentListRecent(articleId, recentPageCount) {
     const commentList = await getCommentList(articleId, 1);
 
@@ -142,6 +221,11 @@ export async function getCommentListRecent(articleId, recentPageCount) {
     return commentListRecent;
 }
 
+/**
+ * 멤버 키로부터 아이디를 가져옵니다.
+ * @param {string} memberKey 멤버 키
+ * @returns {Promise<string>} 멤버 아이디
+ */
 export async function getMemberIdByMemberKey(memberKey) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-mobile/CafeMemberProfile?cafeId=27842958&memberKey=${memberKey}`);
     const json = await response.json();
@@ -149,6 +233,11 @@ export async function getMemberIdByMemberKey(memberKey) {
     return json.message.result.memberId;
 }
 
+/**
+ * 멤버 닉네임으로부터 아이디를 가져옵니다.
+ * @param {string} nickname 멤버 닉네임
+ * @returns {Promise<string>} 멤버 아이디
+ */
 export async function getMemberIdByNickname(nickname) {
     const response = await fetchWithCredentials(`https://apis.naver.com/cafe-web/cafe-talk/v3/categories/27842958/members?query=${nickname}`);
     const json = await response.json();
@@ -167,18 +256,32 @@ export async function getMemberIdByNickname(nickname) {
     return memberList.find(member => member.nickname === nickname)?.memberId;
 }
 
+/**
+ * 멤버 아이디로부터 키를 가져옵니다.
+ * @param {string} memberId 멤버 아이디
+ * @returns {Promise<string>} 멤버 키
+ */
 export async function getMemberKeyByMemberId(memberId) {
     const cafeMemberProfile = await getCafeMemberProfile(memberId);
 
     return cafeMemberProfile.memberKey;
 }
 
+/**
+ * 멤버 아이디로부터 닉네임을 가져옵니다.
+ * @param {string} memberId 멤버 아이디
+ * @returns {Promise<string>} 멤버 닉네임
+ */
 export async function getNicknameByMemberId(memberId) {
     const cafeMemberProfile = await getCafeMemberProfile(memberId);
 
     return cafeMemberProfile.nickName;
 }
 
+/**
+ * 게시판 목록을 가져옵니다.
+ * @returns {Promise<object>} 게시판 목록
+ */
 export async function getMenuList() {
     const response = await fetchWithCredentials('https://apis.naver.com/cafe-web/cafe-cafeinfo-api/v1.0/cafes/27842958/editor/menus');
     const json = await response.json();
@@ -186,6 +289,10 @@ export async function getMenuList() {
     return json.result;
 }
 
+/**
+ * 인기글 목록을 가져옵니다.
+ * @returns {Promise<object>} 인기글 목록
+ */
 export async function getPopularArticleList() {
     const response = await fetchWithCredentials('https://apis.naver.com/cafe-web/cafe2/WeeklyPopularArticleList.json?cafeId=27842958');
     const json = await response.json();
